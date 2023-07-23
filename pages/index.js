e143e87028d319f50aa9e2c253cdde218259f128
SejalChaudhary29 <sejalch2933@gmail.com>
2023-07-23 21:19:59 +0530

    done with header and footer section

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import HeroBanner from "@/components/HeroBanner"
+
+
+
+export default function Home() {
+  return (
+   <>
+   <main className=" h-[500px]">
+    <HeroBanner/>
+   </main>
+   </>
+  )
+}
